b7f70faf6192e65d9eeb93d2a4d8c1978a5c7306
rivindu-yl <kumarage.rivindu@yaalalabs.com>
2026-05-02 00:06:59 +0530

    updated system level stuffs

---
+++ system/systemd/asus-sleep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+case "$1" in
+    pre)
+        # kill keyboard backlight
+        echo 0 > /sys/class/leds/asus::kbd_backlight/brightness
+        # stop asusd before sleep
+        systemctl stop asusd
+        ;;
+    post)
+        # restart asusd on wake
+        systemctl start asusd
+        ;;
+esac
