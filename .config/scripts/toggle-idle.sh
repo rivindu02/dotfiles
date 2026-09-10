a97b120a187ca9abac7cbc89cc591735a5bd92ad
rivindu02 <kumaragerivindu@gmail.com>
2026-09-11 01:22:11 +0530

    scripts: remove notify-send from toggle-idle (stay awake)

--- .config/scripts/toggle-idle.sh
+++ .config/scripts/toggle-idle.sh
@@ -4,10 +4,8 @@ PIDFILE="${XDG_RUNTIME_DIR:-/tmp}/stay-awake.pid"
 if [ -f "$PIDFILE" ]; then
     kill "$(cat "$PIDFILE")" 2>/dev/null
     rm -f "$PIDFILE"
-    notify-send "Stay awake" "Off"
 else
     systemd-inhibit --what=idle:sleep --who="stay-awake-toggle" --why="manual toggle" sleep infinity &
     echo $! > "$PIDFILE"
-    notify-send "Stay awake" "On"
 fi
 pkill -RTMIN+8 waybar
